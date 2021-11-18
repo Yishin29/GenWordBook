00f4c4dca52dd583fd051d136ef5b3f74313b644
Yishin29 <zanku148@daum.net>
2021-11-18 10:06:14 +0900

    NLP 전처리(토큰화, 불용어, 길이1 '포함') 완료

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import pprint
+import nltk
 import requests
 from bs4 import BeautifulSoup
 
-import pprint
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
 #미드 대본 수집
 url = 'https://fangj.github.io/friends/season/0101.html'
@@ -32,6 +35,8 @@ result = requests.get(url)
 doc = BeautifulSoup(result.text, 'html.parser')
 
 sentence_list = doc.select('p > font')
+
+words = [] #영단어장
 for i, sentence in enumerate(sentence_list):
     print('============================================')
     sentence_txt = sentence.get_text().strip()
@@ -40,7 +45,44 @@ for i, sentence in enumerate(sentence_list):
     pprint.pprint('1 >> {}'.format(clean_sentence))
 
 
-#단어만 추출(전처리)
+    #단어만 추출(전처리)
+    #conda install -c anaconda nltk
+    #자연어 처리 -> NLTK
+    #1.토큰화
+    token_list = nltk.word_tokenize(clean_sentence)
+    print('2 >> {}'.format(token_list))
+
+    #2.불용어(stopword) 거르기
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords.append(',') #불용어 추가
+    stopwords.append('?')
+    stopwords.append('...')
+
+    clean_list = []
+    for token in token_list:
+        if token not in stopwords:
+            clean_list.append(token)
+    print('3 >>> {}'.format(clean_list))
+
+    #3. 1개짜리 토큰 제거
+    #len_filer_list = []
+    #for token in clean_list:
+    #    if len(token) > 1:
+    #        len_filer_list.append(token)
+
+    #lambda식 활용
+    len_filer_list = list(filter(lambda x: len(x) > 1, clean_list))
+    print('4 >>>> {}'.format(len_filer_list))
+
+    #4. ' 포함된 토큰 제거
+    clean_filter_list = list(filter(lambda x: "'" not in x, len_filer_list))
+    print('5 >>>>> {}'.format(clean_filter_list))
+    words.extend(clean_filter_list) #모든 list 총합해서 하나로
+    #cf. append= 모든 list 나열만 함
+
+
+print(words)
+
 #빈도수 순으로 나열 및 시각화
 #다음 영어사전 단어정보 수집 및 매칭
 #Excel로 저장
