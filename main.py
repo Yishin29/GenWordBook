a0e71b17f08ddd23e270f3cc068681ca1cd03096
Yishin29 <zanku148@daum.net>
2021-11-23 09:51:08 +0900

    중복단어 제거 및 단어장 알파벳 순 정렬 완료

--- main.py
+++ main.py
@@ -1,99 +1,86 @@
+# MAIN PROGRAM
+
 import pprint
-import nltk
 import requests
 from bs4 import BeautifulSoup
+import nltk
 
-#nltk.download('punkt')
-#nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
-#미드 대본 수집
-url = 'https://fangj.github.io/friends/season/0101.html'
-result = requests.get(url)
-doc = BeautifulSoup(result.text, 'html.parser')
-
-sentence_list = doc.select('p > font')
-for i, sentence in enumerate(sentence_list):
-    print('============================================')
-    sentence_txt = sentence.get_text().strip()
-    start_idx = sentence_txt.find(':')
-    clean_sentence = sentence_txt[start_idx+2:]
-    pprint.pprint('1 >> {}'.format(clean_sentence))
-
-
-#단어만 추출(전처리)
-#빈도수 순으로 나열 및 시각화
-#다음 영어사전 단어정보 수집 및 매칭
-#Excel로 저장
-#Wordcloud로 시각화import requests
-from bs4 import BeautifulSoup
-
-import pprint
-
-#미드 대본 수집
+####################
+# 1.미드 대본 수집 #
+####################
 url = 'https://fangj.github.io/friends/season/0101.html'
 result = requests.get(url)
 doc = BeautifulSoup(result.text, 'html.parser')
 
 sentence_list = doc.select('p > font')
 
-words = [] #영단어장
+words = []  # 영단어장
 for i, sentence in enumerate(sentence_list):
-    print('============================================')
+    print('=====================================================================================')
     sentence_txt = sentence.get_text().strip()
     start_idx = sentence_txt.find(':')
     clean_sentence = sentence_txt[start_idx+2:]
-    pprint.pprint('1 >> {}'.format(clean_sentence))
+    pprint.pprint('1 > {}'.format(clean_sentence))
 
-
-    #단어만 추출(전처리)
-    #conda install -c anaconda nltk
-    #자연어 처리 -> NLTK
-    #1.토큰화
+    #########################
+    # 2.단어만 추출(전처리) #
+    #########################
+    # conda install -c anaconda nltk
+    # 자연어 처리 => NLTK
+    # 2-1.토큰화(Tokenization)
     token_list = nltk.word_tokenize(clean_sentence)
     print('2 >> {}'.format(token_list))
 
-    #2.불용어(stopword) 거르기
+    # 2-2.불용어(StopWord) 제거
     stopwords = nltk.corpus.stopwords.words('english')
-    stopwords.append(',') #불용어 추가
-    stopwords.append('?')
-    stopwords.append('.')
     stopwords.append('..')
     stopwords.append('...')
     stopwords.append('.....')
-    stopwords.append('!')
     stopwords.append('``')
-    stopwords.append('--')
 
     clean_list = []
     for token in token_list:
-        if token not in stopwords:
+        if token.lower() not in stopwords:
             clean_list.append(token)
     print('3 >>> {}'.format(clean_list))
 
-    #3. 1개짜리 토큰 제거
+    # 2-3. Length 1 이하인 token 제거
     #len_filer_list = []
     #for token in clean_list:
     #    if len(token) > 1:
     #        len_filer_list.append(token)
 
-    #lambda식 활용
-    len_filer_list = list(filter(lambda x: len(x) > 1, clean_list))
-    print('4 >>>> {}'.format(len_filer_list))
+    # lambda식을 활용한 Code
+    len_filter_list = list(filter(lambda x: len(x) > 1, clean_list))
+    print('4 >>>> {}'.format(len_filter_list))
 
-    #4. ' 포함된 토큰 제거
-    clean_filter_list = list(filter(lambda x: "'" not in x, len_filer_list))
+    # 2-4.(')포함 된 Token 제거
+    clean_filter_list = list(filter(lambda x: "'" not in x, len_filter_list))
     print('5 >>>>> {}'.format(clean_filter_list))
-    words.extend(clean_filter_list) #모든 list 총합해서 하나로
-    #cf. append= 모든 list 나열만 함
 
-    #5. '-' 포함 토큰 제거
+    # 2-5.('-')포함 된 Token 제거
     clean_list = list(filter(lambda x: "-" not in x, clean_filter_list))
     print('6 >>>>>> {}'.format(clean_list))
     words.extend(clean_list)
 
-print(words)
-
 #빈도수 순으로 나열 및 시각화
+print('===========================')
+print('Total word count: {}'.format(len(words)))
+
+#전체 단어 수 출력
+print('word count after removing duplicates: {}'.format(len(list(set(words)))))
+
+unique_words = sorted(list(set(words)))
+print(unique_words)
+
+#빈도수 가장 높은 단어 1~20위 출력
+text = nltk.Text(words, name='NMSC') #중복 제거 전 단어장 입력으로 사용
+
+pprint.pprint(text.vocab().most_common(20))
+
 #다음 영어사전 단어정보 수집 및 매칭
 #Excel로 저장
 #Wordcloud로 시각화
